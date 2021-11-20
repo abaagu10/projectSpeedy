9eb77cd7367138dec940a03d06b83f082e2de2ad
abaagu10 <76157233+abaagu10@users.noreply.github.com>
2021-11-20 05:40:27 -0800

    wonderful website
    first commint

---
+++ index.js
@@ -0,0 +1,12 @@
+let big=document.getElementsByClassName('dot1');
+let pinki=document.querySelector('.dot2');
+let yellow=document.getElementsByClassName('dot3');
+let white=document.getElementsByClassName('dot4');
+let rollerSection=document.getElementsByClassName('main');
+let shoppingCart=document.getElementsByClassName('sidebar-left');
+let box=document.getElementsByClassName('container');
+//dom
+
+pinki.onclick= function () {
+  box.style= "red";  
+};
